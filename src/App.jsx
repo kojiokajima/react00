f7729b347c9f6eabbf3f7d98316d601715505b7e
wanko <kouji124@w4.dion.ne.jp>
2021-02-28 23:04:52 -0800

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import { Card } from './views/components/index'
+
+const  App = () => {
+  return (
+    <div>
+      <h1>yo</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/components/ButtonBasic.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const ButtonBasic = (props) => {
+    return (
+        // <button>{props.name}</button>
+        <Button variant="contained" color="primary">
+            {/* {props.name} */}
+            ypp
+      </Button>
+    )
+}
+
+
+export default ButtonBasic
